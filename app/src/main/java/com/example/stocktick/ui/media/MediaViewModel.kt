2e1cddefb0c0d61ed563975872e6a0a43bcfb32a
kush <kush_1901me37@iitp.ac.in>
2022-01-30 01:05:43 +0530

    media and education made

--- app/src/main/java/com/example/stocktick/ui/media/MediaViewModel.kt
+++ app/src/main/java/com/example/stocktick/ui/media/MediaViewModel.kt
@@ -1,11 +1,11 @@
-package com.example.stocktick.ui.notifications
+package com.example.stocktick.ui.media
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.LiveData
 
-class NotificationsViewModel(context: Context) : ViewModel() {
+class MediaViewModel(context: Context) : ViewModel() {
     private val _mText: MutableLiveData<String> = MutableLiveData()
     val mText: LiveData<String>
         get() = _mText
