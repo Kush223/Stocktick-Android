cec4e845d5400bdcbe8dda4e330d91a09460cc2f
Ana2k <anushkachakra@gmail.com>
2022-02-28 18:03:27 +0530

    imageview update

--- app/src/main/java/com/example/stocktick/ui/education/BlogAdapter.kt
+++ app/src/main/java/com/example/stocktick/ui/education/BlogAdapter.kt
@@ -1,11 +1,18 @@
 package com.example.stocktick.ui.education
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.request.target.CustomTarget
+import com.example.stocktick.R
 import com.example.stocktick.databinding.EduBlogItemBinding
 import com.example.stocktick.ui.education.model.BlogItem
 
@@ -36,8 +43,14 @@ class BlogViewHolder(val context: Context,private val binding: EduBlogItemBindin
     fun bind(singleItem: BlogItem) {
         binding.blogLongDesc.text = singleItem.long_desc
         binding.blogShortDesc.text = singleItem.short_desc
-        Glide.with(context).load(singleItem.image_url).into(binding.thumbnail)
-        //set_on_click_listener for
-    }
+        Glide.with(context).load(singleItem.image_url).into(binding.blogImageView)
+        }
+        //set_on_click_listener
 
 }
+
+
+
+//https://stackoverflow.com/questions/44273955/glide-callback-after-success-in-kotlin
+//https://stackoverflow.com/questions/33971626/set-background-image-to-relative-layout-using-glide-in-android
+
