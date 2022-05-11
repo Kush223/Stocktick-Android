8d709396b159b523a447c7296cf2fb99486a6ac6
Samuel Aktar Laskar <samuellaskar3@gmail.com>
2022-05-11 12:50:05 +0530

    Layouts done except goal, sip_top, and unknown calculator

--- app/src/main/java/com/example/stocktick/ui/mutual_funds/calculators/HostActivity.kt
+++ app/src/main/java/com/example/stocktick/ui/mutual_funds/calculators/HostActivity.kt
@@ -1,7 +1,10 @@
 package com.example.stocktick.ui.mutual_funds.calculators
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.stocktick.R
 import com.example.stocktick.databinding.ActivityHost2Binding
 
 class HostActivity : AppCompatActivity() {
@@ -11,5 +14,22 @@ class HostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHost2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title = "Calculators"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater?.inflate(R.menu.help, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.help_button) {
+            // do something here
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 }
