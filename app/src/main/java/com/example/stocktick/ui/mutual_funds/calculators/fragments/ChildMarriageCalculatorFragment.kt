2f9e201f25b6fbc37ba3e1f7c19f0f73d27764a8
Samuel Aktar Laskar <samuellaskar3@gmail.com>
2022-05-11 12:16:15 +0530

    Four calculators layout done, dummy layout for the rest is done

---
+++ app/src/main/java/com/example/stocktick/ui/mutual_funds/calculators/fragments/ChildMarriageCalculatorFragment.kt
@@ -0,0 +1,7 @@
+package com.example.stocktick.ui.mutual_funds.calculators.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.stocktick.R
+
+class ChildMarriageCalculatorFragment: Fragment(R.layout.child_marriage_calculator_fragment_layout) {
+}
---
+++ app/src/main/java/com/example/stocktick/ui/mutual_funds/calculators/fragments/EmiCalculatorFragment.kt
@@ -0,0 +1,7 @@
+package com.example.stocktick.ui.mutual_funds.calculators.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.stocktick.R
+
+class EmiCalculatorFragment : Fragment(R.layout.emi_calculator_fragment_layout){
+}
