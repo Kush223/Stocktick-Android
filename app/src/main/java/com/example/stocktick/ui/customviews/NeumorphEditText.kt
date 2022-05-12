c8a7937af194411c110e7beaecbd42063dcb9e62
Samuel Aktar Laskar <samuellaskar3@gmail.com>
2022-05-12 10:19:16 +0530

    Emi formula integrated

--- app/src/main/java/com/example/stocktick/ui/customviews/NeumorphEditText.kt
+++ app/src/main/java/com/example/stocktick/ui/customviews/NeumorphEditText.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.Color
 import android.text.InputType
-import android.text.TextWatcher
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
@@ -51,6 +50,9 @@ NeumorphEditText(
                 }
                 2 -> {
                     editText.inputType = InputType.TYPE_CLASS_NUMBER
+                }
+                3 -> {
+                    editText.inputType = InputType.TYPE_NUMBER_FLAG_DECIMAL
                 }
             }
 
--- app/src/main/java/com/example/stocktick/ui/mutual_funds/calculators/fragments/ChildEducationCalculatorFragment.kt
+++ app/src/main/java/com/example/stocktick/ui/mutual_funds/calculators/fragments/ChildEducationCalculatorFragment.kt
@@ -1,7 +1,16 @@
 package com.example.stocktick.ui.mutual_funds.calculators.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.stocktick.R
+import com.example.stocktick.databinding.ChildEducationCalculatorFragmentLayoutBinding
 
 class ChildEducationCalculatorFragment : Fragment(R.layout.child_education_calculator_fragment_layout){
+
+    private lateinit var binding: ChildEducationCalculatorFragmentLayoutBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = ChildEducationCalculatorFragmentLayoutBinding.bind(view)
+    }
 }
