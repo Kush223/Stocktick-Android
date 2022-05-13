18b7212e262ffd5d169228177bebaf372fb67622
Samuel Aktar Laskar <samuellaskar3@gmail.com>
2022-05-13 16:01:48 +0530

    Autofill api connected

---
+++ app/src/main/java/com/example/stocktick/ui/mutual_funds/risk_factor/models/domain_models/UserProfile.kt
@@ -0,0 +1,8 @@
+package com.example.stocktick.ui.mutual_funds.risk_factor.models.domain_models
+
+data class UserProfile(
+    val name: String?,
+    val email: String?,
+    val gender: String?,
+    val age: Int?
+)
---
+++ app/src/main/java/com/example/stocktick/ui/mutual_funds/risk_factor/models/network_models/UserProfileDto.kt
@@ -0,0 +1,10 @@
+package com.example.stocktick.ui.mutual_funds.risk_factor.models.network_models
+
+data class UserProfileDto(
+    val age: Int?,
+    val email: String?,
+    val gender: String?,
+    val name: String?,
+    val phone: String?,
+    val profile_url: String?
+)
