4b49d9192d19106efaedc26e4cfbb3ae15338a19
Samuel Legion <samuellaskar3@gmail.com>
2022-05-23 12:15:21 +0530

    Picked up a MovableFAB from github, modified it to stick to the sides

---
+++ app/src/main/java/com/example/stocktick/ui/customviews/MovableFloatingActionButton.java
@@ -0,0 +1,122 @@
+package com.example.stocktick.ui.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MovableFloatingActionButton extends FloatingActionButton implements View.OnTouchListener {
+
+    private final static float CLICK_DRAG_TOLERANCE = 10; // Often, there will be a slight, unintentional, drag when the user taps the FAB, so we need to account for this.
+
+    private float downRawX, downRawY;
+    private float dX, dY;
+
+    public MovableFloatingActionButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public MovableFloatingActionButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MovableFloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent){
+
+        int action = motionEvent.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+
+            downRawX = motionEvent.getRawX();
+            downRawY = motionEvent.getRawY();
+            dX = view.getX() - downRawX;
+            dY = view.getY() - downRawY;
+
+            return true; // Consumed
+
+        }
+        //So here is the main logic, let me play with it. I don't want it to stay at the center of the screen, but I want it to
+        //return back to the side which is closest
+        else if (action == MotionEvent.ACTION_MOVE) {
+
+            int viewWidth = view.getWidth();
+            int viewHeight = view.getHeight();
+
+            View viewParent = (View)view.getParent();
+            int parentWidth = viewParent.getWidth();
+            int parentHeight = viewParent.getHeight();
+
+            float newX = motionEvent.getRawX() + dX;
+            newX = Math.max(0, newX); // Don't allow the FAB past the left hand side of the parent
+            newX = Math.min(parentWidth - viewWidth, newX); // Don't allow the FAB past the right hand side of the parent
+
+            float newY = motionEvent.getRawY() + dY;
+            newY = Math.max(0, newY); // Don't allow the FAB past the top of the parent
+            newY = Math.min(parentHeight - viewHeight, newY); // Don't allow the FAB past the bottom of the parent
+
+            view.animate()
+                    .x(newX)
+                    .y(newY)
+                    .setDuration(0)
+                    .start();
+
+            return true; // Consumed
+
+        }
+        else if (action == MotionEvent.ACTION_UP) {
+
+            //Here I am writing the code to stick to the side
+            //Position of the parent view
+            View viewParent = (View)view.getParent();
+            int parentWidth = viewParent.getWidth();
+
+            //Position of the fab
+            float fabPosX = view.getX();
+            float newX;
+            if (fabPosX > parentWidth/2){
+                //Move to the right
+                newX = parentWidth - view.getWidth();
+
+            }
+            else {
+                //move to the left
+                newX = 0f;
+            }
+            view.animate()
+                    .x(newX)
+                    .setDuration(300)
+                    .start();
+
+            float upRawX = motionEvent.getRawX();
+            float upRawY = motionEvent.getRawY();
+
+            float upDX = upRawX - downRawX;
+            float upDY = upRawY - downRawY;
+
+            if (Math.abs(upDX) < CLICK_DRAG_TOLERANCE && Math.abs(upDY) < CLICK_DRAG_TOLERANCE) { // A click
+                return performClick();
+            }
+            else { // A drag
+                return true; // Consumed
+            }
+
+        }
+        else {
+            return super.onTouchEvent(motionEvent);
+        }
+
+    }
+
+}
